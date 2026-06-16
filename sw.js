5f3e345c30b95c53dd10e06f8052b37122131fd4
alexandramllh <alexandra.mallah@gmail.com>
2026-06-16 09:41:10 +0200

    Synchro : convergence vers le cloud sans comparer les horloges des appareils (fix décochage pilule dû au ping-pong/décalage d'horloge)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker — Mon Tableau de Vie
-const CACHE = "mon-tableau-v1781595064";
+const CACHE = "mon-tableau-v1781595670";
 const ASSETS = [
   "/Mon-Tableau-de-Vie/",
   "/Mon-Tableau-de-Vie/index.html",
